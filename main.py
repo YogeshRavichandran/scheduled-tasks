b406e31efc400373cf7cd12dce3ec9b44d35a281
YogeshRavichandran <122593400+YogeshRavichandran@users.noreply.github.com>
2026-05-02 23:44:33 +0530

    Refactor birthday email script for clarity and structure

--- main.py
+++ main.py
@@ -17,22 +17,23 @@ MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 today = datetime.now()
-today_tuple = (today.month, today.day)
+today_tuple = (today.day, today.month)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
+df = pandas.read_csv("./birthdays.csv")
+
+data_dict = {(row.day, row.month): row for (index, row) in df.iterrows()}
+
+if today_tuple in data_dict:
+    birthday_person = data_dict[today_tuple]
+    file_path = f"./letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path, "r") as letter_file:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
         connection.sendmail(
             from_addr=MY_EMAIL,
             to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+            msg=f"Subject: Happy Birthday\n\n{contents}")
